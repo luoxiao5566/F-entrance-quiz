5d59b4d1acf7f2e39310bc7b9d563044bbce02a8
xiao luo <xiao.luo@thoughtworks.com>
2020-10-28 17:24:43 +0800

    [luoxiao] fead: add student function

--- src/App/App.js
+++ src/App/App.js
@@ -3,32 +3,11 @@ import GroupDetail from '../components/GroupDetail/GroupDetail';
 import StudentsDetail from '../components/StudentsDetail/StudentsDetail';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: [],
-    };
-  }
-
-  async componentDidMount() {
-    try {
-      const data = await fetch('http://localhost:8080/students', {
-        method: 'GET',
-        mode: 'cors',
-      });
-      const result = await data.json();
-      console.log(JSON.stringify(result));
-      this.setState({ students: result });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   render() {
     return (
       <div data-testid="app" className="App">
         <GroupDetail />
-        <StudentsDetail students={this.state.students} />
+        <StudentsDetail />
       </div>
     );
   }
--- src/components/StudentsDetail/StudentsDetail.js
+++ src/components/StudentsDetail/StudentsDetail.js
@@ -1,18 +1,95 @@
 import React, { Component } from 'react';
 
 class StudentsDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      visible: true,
+      students: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.getStudent();
+  }
+
+  async getStudent() {
+    try {
+      const data = await fetch('http://localhost:8080/students', {
+        method: 'GET',
+        mode: 'cors',
+      });
+      const result = await data.json();
+      console.log(JSON.stringify(result));
+      this.setState({ students: result });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleKeyDown = async (event) => {
+    if (event.keyCode === 13) {
+      await this.handleAddStudent();
+      this.getStudent();
+      this.setState({
+        name: '',
+        visible: true,
+      });
+    }
+  };
+
+  handleOnClick = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  async handleAddStudent() {
+    try {
+      const data = await fetch('http://localhost:8080/addStudent', {
+        method: 'POST',
+        body: this.state.name,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await data.json();
+      console.log(JSON.stringify(result));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   render() {
     return (
       <div className="studentDetail">
         <h2>学生列表</h2>
         <div className="student">
-          {this.props.students.map((item) => (
+          {this.state.students.map((item) => (
             <div key={item.id}>
               {item.id}.{item.name}
             </div>
           ))}
         </div>
-        <button type="button">+添加学员</button>
+        <button type="button" onClick={this.handleOnClick}>
+          +添加学员
+        </button>
+        <input
+          className="input"
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.name}
+          placeholder="输入姓名按回车键添加"
+          onKeyDown={this.handleKeyDown}
+          hidden={this.state.visible}
+        />
       </div>
     );
   }
